66bed9526f03f5ec40289be21fca05e193833374
skicera <skicera@gmail.com>
2021-02-14 12:33:20 +0200

    initial commit

---
+++ wish-dashboard/js/data.js
@@ -0,0 +1,46 @@
+const user = {
+  name: "Іван",
+  surname: "Шевченко",
+  userPhoto: "my-photo.jpg",
+  qrQode: "qr-code.svg",
+  dreams: [
+    { src: "car.jpg", alt: "Авто моєї мрії" },
+    { src: "home.jpg", alt: "Домівка на місяці" },
+    { src: "voyage.jpg", alt: "Подорож до Марсу" },
+  ],
+};
+
+// arr це середня місячна платня по рокам
+const arr = [
+  500,
+  700,
+  800,
+  1000,
+  1200,
+  1500,
+  1800,
+  2100,
+  2400,
+  2700,
+  3000,
+  3300,
+];
+
+const necessaryExpenses = 800;
+
+const todo = [
+  { month: "Червень", skill: "JavaScript" },
+  { month: "Липень", skill: "Java" },
+  { month: "Серпень", skill: "Python" },
+  { month: "Вересень", skill: "Самостійність" },
+  { month: "Жовтень", skill: "Гнучкість" },
+  { month: "Листопад", skill: "Емоційний інтелект" },
+  { month: "Грудень", skill: "Креативність" },
+  { month: "Січень", skill: "Лідерство" },
+  { month: "Лютий", skill: "Нетворкінг" },
+  { month: "Березень", skill: "Особистий бренд" },
+  { month: "Квітень", skill: "Управління фінансами" },
+  { month: "Травень", skill: "Публічні виступи" },
+];
+
+export { user, arr, necessaryExpenses, todo };
